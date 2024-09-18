017bcddd5caccb30ecd00b26da74873089f1bbd9
voskanyan777 <159440969+voskanyan777@users.noreply.github.com>
2024-09-18 22:10:00 +0700

    API view for update and delete has been added.

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,6 +4,8 @@ from project_management_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/user-cards/lists', UserCardAPIView.as_view(), name='user_card_list'),
-    path('api/v1/user-cards/create', CreateUserCardAPIView.as_view())
+    path('api/v1/user-cards/lists/', UserCardAPIView.as_view(), name='user_card_list'),
+    path('api/v1/user-cards/create/', CreateUserCardAPIView.as_view(), name='user_card_create'),
+    path('api/v1/user-cards/<int:pk>/', UserCardUpdateDeleteAPIView.as_view(), name='user_card_create_and_delete')
+    
 ]
--- project/project_management_app/views.py
+++ project/project_management_app/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
-
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import UserCard
 from .serializers import UserCardSerializer
 
@@ -11,3 +13,22 @@ class UserCardAPIView(generics.ListAPIView):
 class CreateUserCardAPIView(generics.CreateAPIView):
     queryset = UserCard.objects.all()
     serializer_class = UserCardSerializer
+
+
+class UserCardUpdateDeleteAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(UserCard, pk=pk)
+    
+    def put(self, request, pk):
+        user_card = self.get_object(pk=pk)
+        serializer = UserCardSerializer(user_card, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        user_card = self.get_object(pk)
+        user_card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
